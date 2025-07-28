fd5e89abd56eaac9d6418c8964571768f0d1096f
Warda Anwar <153942338+wardaanwar2555@users.noreply.github.com>
2025-07-28 17:28:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -130,4 +130,5 @@ def checkout(): return render_template('checkout.html')
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
